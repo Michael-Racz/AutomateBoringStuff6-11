e1d526e98c6e3a22ebe4804f91c33ff732c96a90
Michael Racz <39655509+Michael-Racz@users.noreply.github.com>
2020-07-12 14:13:28 -0500

    Update factorialLog.py

--- factorialLog.py
+++ factorialLog.py
@@ -1,27 +1,26 @@
 '''
-ABSWP CH11
-boxPrint.py
+factorialLog.py
+ABSWP CH 11 
 Racz 07/07/2020
-Description: This program will print a box in the output. It is a test for 'raise Exception()'.
-    This would be useful to stop a program if a user gives a valid input but does NOT crash the program.
-    In it, the program 'knows' how to handle the exception.
+
+Description: This program calculates the factorial of a number but implements logging to show its use.
+    We put in 'logging.debug()' when we want to print log information.
 
 '''
 
+import logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s \
+-  %(message)s')
 
-def boxPrint(symbol, width, height):
-    if len(symbol) != 1:
-        raise Exception('Symbol must be a single character string.')
-    if width <=2:
-        raise Exception('Width must be greater than 2.')
-    if height <= 2:
-        raise Exception('Height must be greater than 2.')
-    print(symbol*width)
-    for i in range(height - 2):
-        print(symbol + (' ' * (width - 2)) + symbol)
-    print(symbol * width)
-for sym,w,h in (('*', 4,4),('O',20,5),('x',1,3),('ZZ',3,3)):
-    try:
-        boxPrint(sym,w,h)
-    except Exception as err:
-        print('An exception happened: ' + str(err))
+logging.debug('Start of program')
+def factorial(n):
+    logging.debug('Start of factorial(%s%%)' % (n))
+    total = 1
+    for i in range(n+1):
+        total *= i
+        logging.debug('i is ' + str(i) + ' , total is ' + str(total))
+    logging.debug('End of factorial(%s%%)' % (n))
+    return total
+
+print(factorial(5))
+logging.debug('End of program')
